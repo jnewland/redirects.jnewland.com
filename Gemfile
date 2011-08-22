ae846b259b9253e4b1a8d8188e6e9f33ab0963de
Jesse Newland <jesse@jnewland.com>
2011-08-22 09:47:51 -0400

    redirect to jnewland.github.com

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source :rubyforge
 
 gem 'heroku'
 gem 'foreman'
+gem 'refraction'
 gem 'sinatra', '1.1.0'
 gem 'thin', '1.2.7'
--- redirects.rb
+++ redirects.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'refraction'
 
-get '/' do
-  "Hello, world"
+use Refraction
+
+Refraction.configure do |req|
+
+  case req.path
+  when false
+    # other stuff
+  else
+    req.permanent! "http://jnewland.github.com#{req.path}"
+  end
+
 end
